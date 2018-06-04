6f8df552b4a5b5d1163230cd34a8425f3cb5ae4e
tuanis@live.com <mac@MacBook-Pro-FGT.local>
2018-06-04 23:58:52 +0200

    added funtionality to temp

--- resources/js/javaScript.js
+++ resources/js/javaScript.js
@@ -1,24 +1,40 @@
-//Miles to KM converter
-
-//Function 
+//Function KM to Miles
 
 function mEquation() {
 	var miles = .62;
-	var x = document.getElementById("mInput").value;
-    var totalMiles = miles * x;
-    var n = totalMiles.toFixed(1);
-    document.getElementById("kInput").value = n;
+	var k = document.getElementById("kInput").value;
+    var totalMiles = miles * k;
+    var m = totalMiles.toFixed(1); //Result shows only one decimal
+    document.getElementById("mInput").value = m;
 }
 
+//Function Miles to KM 
 function kEquation() {
 	var kilometers = 1.60;
-	var x = document.getElementById("kInput").value;
-    var totalMiles = x * kilometers;
-    var n = totalMiles.toFixed(1);
-    document.getElementById("mInput").value = n;
+	var m = document.getElementById("mInput").value;
+    var totalKm = m * kilometers; 
+    var km = totalKm.toFixed(1); //Result shows only one decimal
+    document.getElementById("kInput").value = km;
 }
 
+//Clear input fields
 function refreshFunc() {
     document.getElementById('mInput').value = '';
     document.getElementById('kInput').value = '';
+}
+
+//Function Celsius to Fahrenheit
+function fEquation() {
+	var f = document.getElementById("mInput").value;
+    var totalC = (f - 32) * 5/9;
+    var c = totalC.toFixed(1); //Result shows only one decimal
+    document.getElementById("kInput").value = c;
+}
+
+//Function Fahrenheit to Celsius
+function cEquation() {
+	var i = document.getElementById("kInput").value;
+    var totalF = i * 9/5 + 32; 
+    var f = totalF.toFixed(1); //Result shows only one decimal
+    document.getElementById("mInput").value = f;
 }
