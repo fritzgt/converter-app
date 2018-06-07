881986b99113300dc43395f50596e60407f38f9a
tuanis@live.com <mac@MacBook-Pro-FGT.fritz.box>
2018-06-07 23:52:25 +0200

    added posible fix for ios links

--- resources/js/javaScript.js
+++ resources/js/javaScript.js
@@ -96,6 +96,21 @@ function refreshFunc() {
     document.getElementById('kInput').value = '';
 }
 
+// iOS webapp links
+
+$(document).ready(function(){
+    if (("standalone" in window.navigator) && window.navigator.standalone) {
+      // For iOS Apps
+      $('a').on('click', function(e){
+        e.preventDefault();
+        var new_location = $(this).attr('href');
+        if (new_location != undefined && new_location.substr(0, 1) != '#' && $(this).attr('data-method') == undefined){
+          window.location = new_location;
+        }
+      });
+    }
+  });
+
 
 
 //Currency converter API
